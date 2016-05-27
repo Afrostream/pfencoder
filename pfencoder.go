9c0a545e509de877eda8b28a64a2a6a3685338fa
Ubuntu <spebsd@gmail.com>
2016-05-27 16:45:22 +0200

    A lot of modification since last time :)

--- pfencoder.go
+++ pfencoder.go
@@ -6,10 +6,10 @@ import (
         "path"
 	"errors"
 	"fmt"
-//	"strings"
+	"strings"
+        "net/http"
 	"log"
 	"time"
-//	"strconv"
 	"regexp"
 	"encoding/json"
 	"github.com/streadway/amqp"
@@ -20,6 +20,7 @@ import (
 var ffmpegProcesses int
 
 const ffmpegPath = "/usr/bin/ffmpeg"
+const spumuxPath = "/usr/bin/spumux"
 const uptimePath = "/usr/bin/uptime"
 const dbDsn = "pfencoder:Fhd4elKd0UxCd43gVHu5@tcp(10.91.83.18:3306)/video_encoding"
 
@@ -38,9 +39,8 @@ type Preset struct {
 }
 
 type AssetConfiguration struct {
-  Uuid		string
-  SrcFilename	string
-  DstFilename	string
+  SrcFilename	*string
+  DstFilename	*string
   P		Preset
 }
 
@@ -165,111 +165,183 @@ func dbSetFFmpegLog(db *sql.DB, assetId int, fullLog string) {
   }
 }
 
+func updateAssetsStreams(assetId int) {
+  url := fmt.Sprintf("http://p-afsmsch-001.afrostream.tv:4000/api/assetsStreams/%d", assetId)
+  _, err := http.Post(url, "application/json", strings.NewReader("{}"))
+  if err != nil {
+    log.Printf("XX Cannot update assetsStreams with url %s: %s", url, err)
+    return
+  }
+
+  return
+}
+
 func doEncoding(assetId int) {
   log.Printf("-- [ %d ] Encoding task received", assetId)
   log.Printf("-- [ %d ] Get asset encoding configuration from database", assetId)
   db := openDb()
   defer db.Close()
-  query := "SELECT c.contentId,c.uuid,c.filename,a.filename,p.presetId,p.profileId,p.type,p.cmdLine,p.createdAt,p.updatedAt FROM presets AS p LEFT JOIN assets AS a ON p.presetId=a.presetId, contents AS c LEFT JOIN assets AS a2 ON c.contentId=a2.contentId WHERE a.assetId=?"
+  query := "SELECT c.contentId,c.uuid,c.filename,a2.filename,a.filename,p.presetId,p.profileId,p.type,p.cmdLine,p.createdAt,p.updatedAt FROM assets AS a LEFT JOIN presets AS p ON a.presetId=p.presetId LEFT JOIN assets AS a2 ON a.assetIdDependance=a2.assetId LEFT JOIN contents AS c ON c.contentId=a.contentId WHERE a.assetId=?"
   stmt, err := db.Prepare(query)
   if err != nil {
     dbSetAssetStatus(db, assetId, "failed")
     log.Printf("XX [ %d ] Cannot prepare query %s: %s", assetId, query, err)
     return
   }
+  defer stmt.Close()
   var ac AssetConfiguration
-  var contentId int
-  err = stmt.QueryRow(assetId).Scan(&contentId, &ac.Uuid, &ac.SrcFilename, &ac.DstFilename, &ac.P.Id, &ac.P.ProfileId, &ac.P.Type, &ac.P.CmdLine, &ac.P.CreatedAt, &ac.P.UpdatedAt)
+  var contentId *int
+  var uuid *string
+  var contentFilename *string
+  err = stmt.QueryRow(assetId).Scan(&contentId, &uuid, &contentFilename, &ac.SrcFilename, &ac.DstFilename, &ac.P.Id, &ac.P.ProfileId, &ac.P.Type, &ac.P.CmdLine, &ac.P.CreatedAt, &ac.P.UpdatedAt)
   if err != nil {
     dbSetAssetStatus(db, assetId, "failed")
     log.Printf("XX [ %d ] Cannot query %s with assetId=%d and scan results: %s", assetId, query, assetId, err)
     return
   }
-  dir := path.Dir(ac.DstFilename)
-  err = os.MkdirAll(dir, os.ModeDir)
+  if ac.SrcFilename == nil {
+    ac.SrcFilename = contentFilename
+  }
+  dir := path.Dir(*ac.DstFilename)
+  err = os.MkdirAll(dir, 0755)
   if err != nil {
     log.Printf("XX Cannot create directory %s: %s", dir, err)
     dbSetAssetStatus(db, assetId, "failed")
     return
   }
 
-  var ffmpegArgs []string
-  bitrate := strconv.Itoa(ac.P.Bitrate)
-  gop := strconv.Itoa(ac.P.Gop)
-  if ac.P.Type == "video" {
-    ffmpegArgs = []string{ "-i", ac.SrcFilename, "-y", "-vf", `scale=426:240,setsar=1/1`, "-c:v", ac.P.Codec, "-preset:v", "fast", "-profile:v", ac.P.CodecProfile, "-level", ac.P.CodecLevel, "-b:v", bitrate + "k", "-minrate", bitrate + "k", "-maxrate", bitrate + "k", "-bufsize", bitrate + "k", "-g", gop, "-keyint_min", gop, "-sc_threshold", "0", "-pix_fmt", "yuv420p", "-map", "0:v", "-an", "-map_chapters", "-1", "-threads", "0", ac.DstFilename }
+  var cmdArgs []string
+  cmdLine := strings.Replace(ac.P.CmdLine, "%SOURCE%", *ac.SrcFilename, -1)
+  cmdLine = strings.Replace(cmdLine, "%DESTINATION%", *ac.DstFilename, -1)
+  cmdLine = strings.Replace(cmdLine, "%UUID%", *uuid, -1)
+  cmdLine = strings.Replace(cmdLine, "%BASEDIR%", path.Dir(*ac.DstFilename), -1)
+  log.Printf("uuid is %s", *uuid)
+  query = "SELECT parameter,value FROM profilesParameters WHERE assetId=?"
+  stmt, err = db.Prepare(query)
+  if err != nil {
+    dbSetAssetStatus(db, assetId, "failed")
+    log.Printf("XX [ %d ] Cannot prepare query %s: %s", query, err)
+    return
   }
-  if ac.P.Type == "audio" {
-    ffmpegArgs = []string{ "-i", ac.SrcFilename, "-y", "-vn", "-acodec", ac.P.Codec, "-ac", "2", "-b:a", bitrate + "k", "-ar", "48000", "-map", "0:a:0", "-map_chapters", "-1", "-threads", "0", ac.DstFilename }
+  defer stmt.Close()
+  var rows *sql.Rows
+  rows, err = stmt.Query(assetId)
+  if err != nil {
+    dbSetAssetStatus(db, assetId, "failed")
+    log.Printf("XX [ %d ] Cannot query %s with (%d): %s", query, assetId, err)
+    return
+  }
+  defer rows.Close()
+  for rows.Next() {
+    var parameter string
+    var value string
+    err = rows.Scan(&parameter, &value)
+    if err != nil {
+      dbSetAssetStatus(db, assetId, "failed")
+      log.Printf("XX [ %d ] Cannot scan result for query %s: %s", query, err)
+      return
+    }
+    cmdLine = strings.Replace(cmdLine, `%` + parameter + `%`, value, -1)
+  }
+  cmdArgs = strings.Split(cmdLine, " ")
+  var binaryPath string
+  switch(ac.P.Type) {
+    case "ffmpeg":
+      binaryPath = ffmpegPath
+    case "spumux":
+      binaryPath = spumuxPath
+    case "script":
+      binaryPath = cmdArgs[0]
+      cmdArgs = cmdArgs[1:]
   }
 
-  cmd := exec.Command(ffmpegPath, ffmpegArgs...)
+  cmd := exec.Command(binaryPath, cmdArgs...)
   stderr, err := cmd.StderrPipe()
   ffmpegProcesses++
-  log.Printf("-- [ %d ] Running command: %s %s", assetId, ffmpegPath, strings.Join(ffmpegArgs, " "))
+  log.Printf("-- [ %d ] Running command: %s %s", assetId, binaryPath, strings.Join(cmdArgs, " "))
   err = cmd.Start()
   if err != nil {
     dbSetAssetStatus(db, assetId, "failed")
-    dbSetContentStatus(db, contentId, "failed")
-    log.Fatalf("-- [ %d ] Cannot start command %s %s: %s", assetId, ffmpegPath, strings.Join(ffmpegArgs, " "), err)
+    if contentId != nil {
+      dbSetContentStatus(db, *contentId, "failed")
+    }
+    log.Printf("-- [ %d ] Cannot start command %s %s: %s", assetId, binaryPath, strings.Join(cmdArgs, " "), err)
     return
   }
   dbSetAssetStatus(db, assetId, "processing")
 
-  var s string
-  b := make([]byte, 32)
-  ffmpegStartOK := false
-  for {
-    bytesRead, err := stderr.Read(b);
-    if err != nil {
-      break
-    }
-    if strings.Contains(s, `Press [q] to stop, [?] for help`) == true {
-      ffmpegStartOK = true
-      break
-    }
-    s += string(b[:bytesRead])
-  }
-
-  // If FFMpeg exit with error
-  if ffmpegStartOK == false {
-    dbSetAssetStatus(db, assetId, "failed")
-    dbSetFFmpegLog(db, assetId, s)
-    log.Printf("XX [ %d ] FFMpeg execution error, please consult logs in database table 'logs'", assetId)
-    return
-  }
-
-  re, err := regexp.Compile(`frame= *([0-9]*) *fps= *([0-9]*) *q= *([-0-9\.]*)* *L?size= *([0-9]*)kB *time= *([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{2}) *bitrate= *([0-9\.]*)kbits/s`)
-  if err != nil {
-    log.Printf("XX [ %d ] Cannot compile regexp frame=([0-9]*), progression will not be available: %s", assetId, err)
-  }
-
   fullLog := ""
-  for {
-    bytesRead, err := stderr.Read(b)
-    if err != nil {
-      s += string([]byte{ 0x0d })
-    }
-    s += string(b[:bytesRead])
-    if strings.Contains(s, string([]byte{ 0x0d })) == true {
-      str := strings.Split(s, string([]byte{ 0x0d }))
-      fullLog += str[0] + string([]byte{ 0x0a }) + str[1]
-      s = str[1]
-      matches := re.FindAllStringSubmatch(str[0], -1)
-      for _, v := range matches {
-        var fp FFMpegProgression
-        fp.Frame = v[1]
-        fp.Fps = v[2]
-        fp.Q = v[3]
-        fp.Size = v[4]
-        fp.Elapsed = strings.Split(v[5], ".")[0]
-        fp.Bitrate = v[6]
-        dbSetFFmpegProgression(db, assetId, fp)
+  switch(ac.P.Type) {
+    case "ffmpeg":
+      var s string
+      b := make([]byte, 32)
+      ffmpegStartOK := false
+      for {
+        bytesRead, err := stderr.Read(b);
+        if err != nil {
+          break
+        }
+        if strings.Contains(s, `Press [q] to stop, [?] for help`) == true {
+          ffmpegStartOK = true
+          break
+        }
+        s += string(b[:bytesRead])
       }
-    }
-    if err != nil {
-      break
-    }
+
+      // If FFMpeg exit with error
+      if ffmpegStartOK == false {
+        dbSetAssetStatus(db, assetId, "failed")
+        dbSetFFmpegLog(db, assetId, s)
+        log.Printf("XX [ %d ] FFMpeg execution error, please consult logs in database table 'logs'", assetId)
+        cmd.Wait()
+        ffmpegProcesses--
+        return
+      }
+
+      re, err := regexp.Compile(`frame= *([0-9]*) *fps= *([0-9]*) *q= *([-0-9\.]*)* *L?size= *([0-9]*)kB *time= *([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{2}) *bitrate= *([0-9\.]*)kbits/s`)
+      if err != nil {
+        log.Printf("XX [ %d ] Cannot compile regexp frame=([0-9]*), progression will not be available: %s", assetId, err)
+      }
+
+      fullLog = ""
+      for {
+        bytesRead, err := stderr.Read(b)
+        if err != nil {
+          s += string([]byte{ 0x0d })
+        }
+        s += string(b[:bytesRead])
+        if strings.Contains(s, string([]byte{ 0x0d })) == true {
+          str := strings.Split(s, string([]byte{ 0x0d }))
+          fullLog += str[0] + string([]byte{ 0x0a }) + str[1]
+          s = str[1]
+          matches := re.FindAllStringSubmatch(str[0], -1)
+          for _, v := range matches {
+            var fp FFMpegProgression
+            fp.Frame = v[1]
+            fp.Fps = v[2]
+            fp.Q = v[3]
+            fp.Size = v[4]
+            fp.Elapsed = strings.Split(v[5], ".")[0]
+            fp.Bitrate = v[6]
+            dbSetFFmpegProgression(db, assetId, fp)
+          }
+        }
+        if err != nil {
+          break
+        }
+      }
+    case "spumux":
+      fallthrough
+    case "script":
+      b := make([]byte, 4096)
+      fullLog = ""
+      for {
+        bytesRead, err := stderr.Read(b)
+        if err != nil {
+          break;
+        }
+        fullLog += string(b[:bytesRead])
+      }
   }
   err = cmd.Wait()
   if err != nil {
@@ -277,12 +349,13 @@ func doEncoding(assetId int) {
     log.Printf("XX [ %d ] FFMpeg execution error, please consult logs in database table 'logs'", assetId)
   } else {
     dbSetAssetStatus(db, assetId, "ready")
+    if ac.P.Type == "ffmpeg" {
+      updateAssetsStreams(assetId)
+    }
   }
   ffmpegProcesses--
   dbSetFFmpegLog(db, assetId, fullLog)
-  log.Printf("-- [ %d ] FFmpeg result: %s", assetId, fullLog)
-  log.Printf("-- [ %d ] FFMpeg encoding done sucessfully", assetId)
-  log.Printf("-- [ %d ] Asset configuration is %#v", assetId, ac)
+  log.Printf("-- [ %d ] FFmpeg execution success", assetId)
 
   return
 }
@@ -327,7 +400,7 @@ func registerEncoder() (id int64, err error) {
       }
     case err != nil:
       stmt.Close()
-      log.Fatalf("Error during query execution %s with hostname=%s: %s", query, hostname, err)
+      log.Printf("Error during query execution %s with hostname=%s: %s", query, hostname, err)
   }
 
   return
@@ -380,9 +453,11 @@ func startMonitoringLoad(encoderId int64) {
       _, err = stmt.Exec(load1, ffmpegProcesses, encoderId)
       if err != nil {
         log.Printf("XX Can't exec query %s with (%s): %s", query, load1, err)
+        stmt.Close()
         db.Close()
         continue
       }
+      stmt.Close()
       db.Close()
     }
   }()
@@ -449,6 +524,8 @@ func main() {
 
   startMonitoringLoad(encoderId)
 
+  forever := make(chan bool)
+
   go func() {
     for d := range msgs {
       log.Printf("Received a message: %s", d.Body)
@@ -472,5 +549,5 @@ func main() {
   }()
 
   log.Printf(" [*] Waiting for messages, To exit press CTRL+C")
-  time.Sleep(1000 * time.Second)
+  <-forever
 }
